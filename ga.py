c028ba2a0006e408fa84e04e17b30ac5359355f5
Gil Tankus <gtankus@akamai.com>
2019-03-07 15:38:50 +0200

    Major bugs fixed and added GA statistics

--- ga.py
+++ ga.py
@@ -1,63 +1,94 @@
+import datetime
 import copy
 import random
+import matplotlib
+matplotlib.use('TkAgg')
+
+import matplotlib.pyplot as plt
+from collections import defaultdict
 from network import AdHocSensorNetwork
 
 
 class Agent(object):
 
-    def __init__(self, network):
+    def __init__(self, network, fitness_function):
         self.network = network
+        self.fitness_function = fitness_function
         self.fitness = 0
         self.calc_fitness()
 
     def calc_fitness(self):
-        is_connected = self.network.graph.is_connected()
-        if is_connected:
-            self.fitness = 10 * len(self.network.graph.edges)
-        else:
-            self.fitness = len(self.network.graph.edges)
+        self.fitness = self.fitness_function(network=self.network)
 
     def __lt__(self, other):
         return self.fitness > other.fitness
 
 
+class GAStatistics(object):
+
+    GEN_FITNESS = 'Gen-Fitness'
+    GEN_TIME = 'Gen-Time'
+
+    def __init__(self, ga):
+        self.ga = ga
+        self.statistics = defaultdict(list)
+
+    def gen_snapshot(self, gen, time_spent):
+        self.statistics[GAStatistics.GEN_FITNESS].append((gen, max([agent.fitness for agent in self.ga.agents])))
+        self.statistics[GAStatistics.GEN_TIME].append((gen, time_spent))
+
+    def plot_statistic(self, name):
+        statistic = self.statistics.get(name)
+        if statistic:
+            plt.figure(name)
+            ax = plt.gca()
+            xs = list(map(lambda p: p[0], statistic))
+            ys = list(map(lambda p: p[1], statistic))
+            ax.scatter(xs, ys, s=5, c='red', alpha=1)
+            ax.set_title(name)
+            plt.show()
+        else:
+            print("No statistic with name {} was found".format(name))
+
+
 class GA(object):
 
-    def __init__(self, interest_areas, initial_population_size, generations, mutation_factor=0.2, death_factor=0.2):
+    def __init__(self, interest_areas, initial_population_size, generations, fitness_function, mutation_factor=0.2, death_factor=0.2):
         self.interest_areas = interest_areas
         self.initial_population_size = initial_population_size
-        self.agents = self.generate_initial_population()
+        self.fitness_function = fitness_function
+        self.agents = self.generate_initial_population(fitness_function=self.fitness_function)
         self.generations = generations
         self.mutation_factor = mutation_factor
         self.death_factor = death_factor
+        self.statistics = GAStatistics(ga=self)
 
-    def generate_initial_population(self):
+    def generate_initial_population(self, fitness_function):
         agents = list()
         for i in range(self.initial_population_size):
             network = AdHocSensorNetwork(interest_areas=self.interest_areas)
             network.randomize()
-            agents.append(Agent(network=network))
+            agents.append(Agent(network=network, fitness_function=fitness_function))
         return agents
 
     def evolve(self):
         for gen in range(self.generations):
+            start = datetime.datetime.now()
             print("Generation: " + str(gen))
             self.calc_fitness()
             self.selection()
             self.breed()
             self.mutate()
+            self.statistics.gen_snapshot(gen=gen, time_spent=(datetime.datetime.now() - start).total_seconds())
         self.calc_fitness()
-        return True
 
     def calc_fitness(self):
         for agent in self.agents:
             agent.calc_fitness()
 
     def selection(self):
-        if len(self.agents) == 1:
-            return
         selected_agents = sorted(self.agents, key=lambda agent: agent.fitness, reverse=True)
-        self.agents = selected_agents[:int((1 - self.death_factor) * len(selected_agents))]
+        self.agents = selected_agents[:self.initial_population_size]
 
     def breed(self):
         all_agents = set(self.agents)
@@ -75,9 +106,9 @@ class GA(object):
             all_a1_nodes = list(a1.network.graph.nodes.values())
             all_a2_nodes = list(a2.network.graph.nodes.values())
 
-            a1_nodes = set(random.choices(all_a1_nodes, k=random.randint(0, len(all_a1_nodes))))
+            a1_nodes = set(random.choices(all_a1_nodes, k=random.randint(0, int(len(all_a1_nodes) / 2))))
             compliment_in_a2 = set(node for node in filter(lambda n: n not in a1_nodes, all_a2_nodes))
-            a2_nodes = set(random.choices(all_a2_nodes, k=random.randint(0, len(all_a2_nodes))))
+            a2_nodes = set(random.choices(all_a2_nodes, k=random.randint(0, int(len(all_a2_nodes) / 2))))
             compliment_in_a1 = set(node for node in filter(lambda n: n not in a2_nodes, all_a1_nodes))
 
             first_born_nodes = set(map(lambda n: copy.deepcopy(n), a1_nodes.union(compliment_in_a2)))
@@ -85,27 +116,30 @@ class GA(object):
 
             second_born_nodes = set(map(lambda n: copy.deepcopy(n), a2_nodes.union(compliment_in_a1)))
             second_born = AdHocSensorNetwork(interest_areas=self.interest_areas, nodes=second_born_nodes)
-            offsprings.add(Agent(network=first_born))
-            offsprings.add(Agent(network=second_born))
+            offsprings.add(Agent(network=first_born, fitness_function=self.fitness_function))
+            offsprings.add(Agent(network=second_born, fitness_function=self.fitness_function))
         self.agents.extend(offsprings)
 
     def mutate(self):
         for agent in self.agents:
-            # if random.random() >= self.mutation_factor:
-            #     return
+            if random.random() >= self.mutation_factor:
+                return
             network = agent.network
-            connectivity_components = network.graph.get_connectivity_components()
-            smallest_component = None
-            largest_component = None
-            for component_id in connectivity_components:
-                connectivity_component = connectivity_components[component_id]
-                smallest_component = connectivity_component if smallest_component is None or len(smallest_component) > len(
-                    connectivity_component) else smallest_component
-                largest_component = connectivity_component if largest_component is None or len(largest_component) <= len(
-                    connectivity_component) else largest_component
+            random_node = random.choice(network.graph.nodes)
+            network.move_sensor(random_node.node_id)
 
-            # TODO - check the distance (d) between connectivity components.
-            #        if d <= 1 - try to join components
-            #        if d > 1 - never mind
-            for node in smallest_component:
-                network.move_sensor(node.node_id)
+            # connectivity_components = network.graph.get_connectivity_components()
+            # smallest_component = None
+            # largest_component = None
+            # for component_id in connectivity_components:
+            #     connectivity_component = connectivity_components[component_id]
+            #     smallest_component = connectivity_component if smallest_component is None or len(smallest_component) > len(
+            #         connectivity_component) else smallest_component
+            #     largest_component = connectivity_component if largest_component is None or len(largest_component) <= len(
+            #         connectivity_component) else largest_component
+            #
+            # # TODO - check the distance (d) between connectivity components.
+            # #        if d <= 1 - try to join components
+            # #        if d > 1 - never mind
+            # for node in smallest_component:
+            #     network.move_sensor(node.node_id)
--- simulator.py
+++ simulator.py
@@ -1,28 +1,82 @@
+import random
 import matplotlib
 matplotlib.use('TkAgg')
-
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle as CircleUI
 from network import InterestArea
 from geometry import Point
+from ga import GA, GAStatistics
 
-from ga import GA
+# interest_areas = [
+#     InterestArea(center=Point(0, 0), radius=0.5, name='HUB', is_hub=True),
+#     InterestArea(center=Point(1, 0.5), radius=0.5, name='Omega1'),
+#     InterestArea(center=Point(2, 0), radius=0.5, name='Omeg2'),
+#     InterestArea(center=Point(1, -0.5), radius=0.5, name='Omega3'),
+# ]
 
-interest_areas = [
-    InterestArea(center=Point(0, 0), radius=0.5, name='HUB', is_hub=True),
-    InterestArea(center=Point(1, 0.5), radius=0.5, name='Omega1'),
-    InterestArea(center=Point(2, 0), radius=0.5, name='Omeg2'),
-    InterestArea(center=Point(1, -0.5), radius=0.5, name='Omega3'),
-]
+X_LIMS = (-7, 7)
+Y_LIMS = (-7, 7)
+NUMBER_OF_INTEREST_AREAS = 100
+RUN_GA = True
 
 
-ga = GA(interest_areas=interest_areas, initial_population_size=5, generations=18)
-fig_id = 1
-agents = sorted(ga.agents, key=lambda agent: agent.fitness, reverse=True)
-for agent in agents:
-    network = agent.network
+def generate_interest_areas():
+    interest_areas = set()
+    interest_area_id = 1
+    while len(interest_areas) < NUMBER_OF_INTEREST_AREAS:
+        ia = InterestArea(center=Point(x=(X_LIMS[0] + 1) + (X_LIMS[1] - 1 - X_LIMS[0] - 1) * random.random(),
+                                       y=(Y_LIMS[0] + 1) + (Y_LIMS[1] - 1 - Y_LIMS[0] - 1) * random.random()),
+                          radius=0.5, name='Omega' + str(interest_area_id))
+        if not any(other.intersects(ia) for other in interest_areas):
+            interest_areas.add(ia)
+
+    hub = random.sample(interest_areas, 1)[0]
+    hub.name = 'HUB'
+    hub.is_hub = True
+
+    return interest_areas
+
+
+def edges_fitness_function(network):
+    is_connected = network.graph.is_connected()
+    if is_connected:
+        return 10 * len(network.graph.edges)
+    else:
+        return len(network.graph.edges)
+
+
+def largest_connectivity_componenet_fitness_function(network):
+    connectivity_components = network.graph.get_connectivity_components()
+    return max(map(lambda item: len(item[1]), connectivity_components.items()))
+
+
+interest_areas = generate_interest_areas()
+
+if RUN_GA:
+    # GA - will always mutate (mutation factor = 1)
+    ga = GA(interest_areas=interest_areas, initial_population_size=20, generations=200,
+            fitness_function=largest_connectivity_componenet_fitness_function, mutation_factor=1)
+    fig_id = 1
+    # for agent in ga.agents:
+    #     network = agent.network
+    #     network.plot(fig_id=fig_id, xlims=[-7, 7], ylims=[-7, 7])
+    #     fig_id += 1
+    ga.evolve()
+    agents = sorted(ga.agents, key=lambda agent: agent.fitness, reverse=True)
+    network = agents[0].network
     network.plot(fig_id=fig_id, xlims=[-7, 7], ylims=[-7, 7])
-    fig_id += 1
-ga.evolve()
-agents = sorted(ga.agents, key=lambda agent: agent.fitness, reverse=True)
-network = agents[0].network
-network.plot(fig_id=fig_id, xlims=[-7, 7], ylims=[-7, 7])
-
+    ga.statistics.plot_statistic(name=GAStatistics.GEN_FITNESS)
+    ga.statistics.plot_statistic(name=GAStatistics.GEN_TIME)
+else:
+    plt.figure(1)
+    ax = plt.gca()
+    for ia in interest_areas:
+        color = 'blue' if not ia.is_hub else 'green'
+        c = CircleUI((ia.center.x, ia.center.y), ia.radius, facecolor=color, edgecolor='black')
+        c.set_alpha(0.5)
+        c.set_label(ia)
+        ax.add_artist(c)
+    ax.set_title('Adhoc Network')
+    plt.xlim(X_LIMS[0], X_LIMS[1])
+    plt.ylim(Y_LIMS[0], Y_LIMS[1])
+    plt.show()
