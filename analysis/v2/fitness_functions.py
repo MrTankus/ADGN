fixed harmonic avg fitness function and remove regular average fitness function

analysis/v2/fitness_functions.py
@@ -1,57 +1,56 @@
 import itertools
-import numpy as np
+from scipy.special import comb
 
 
 def sum_square_connectivity_componenet_fitness_function(network):
     return sum(map(lambda cc: len(cc) ** 2, network.graph.get_connectivity_components()))
 
 
-def avg_on_paths_length_fitness_function(network):
-    sensors = filter(lambda vertex: not vertex.get('is_relay'), network.graph.vertices)
-    sensors_pairs = itertools.combinations(sensors, 2)
-    path_distances = [network.graph.get_path_length(v1, v2) for v1, v2 in sensors_pairs]
-    finite_paths = list(filter(lambda d: d != np.inf, path_distances))
-    number_of_finit_paths = len(finite_paths)
-    sum_path_distances = sum(finite_paths)
-    if number_of_finit_paths:
-        number_of_vertices = len(network.graph.vertices)
-        return sum_path_distances / (number_of_vertices * (number_of_vertices - 1))
-    return 0
-
-
 def harmonic_avg_on_paths_length_fitness_function(network):
-    sensors = filter(lambda node: not node.get('is_relay'), network.graph.vertices)
-    sensors_pairs = itertools.combinations(sensors, 2)
-    path_distances = [network.graph.get_path_length(v1, v2) for v1, v2 in sensors_pairs]
-    finite_paths = list(filter(lambda d: d != np.inf, path_distances))
-    path_distances = list(map(lambda d: 1/d, finite_paths))
-    sum_path_distances = sum(path_distances)
-    if sum_path_distances:
-        return float(len(path_distances) / sum_path_distances)
-    return 0
+    n = len(network.graph.vertices)
+    total_combinations = comb(n, 2)
+    distance_sum = list()
+    ccs = network.graph.get_connectivity_components()
+    if len(ccs) == n:
+        return 0
+
+    for cc in network.graph.get_connectivity_components():
+        if len(cc) == 1:
+            continue
+        sensors = filter(lambda vertex: not vertex.get('is_relay'), cc)
+        sensors_pairs = itertools.combinations(sensors, 2)
+        path_distances = [network.graph.get_path_length(v1, v2) for v1, v2 in sensors_pairs]
+        distance_sum.append(sum(map(lambda d: 1.0/d, path_distances)))
+        total_combinations -= len(path_distances)
+
+    return ((n * (n-1)) / 2) / sum(distance_sum)
 
 
-def robustness_fitness_function(network):
-    return 0
+class Optimum(object):
+    MIN = 0
+    MAX = 1
 
 
 class FitnessFunctions(object):
 
     SUM_SQUARE_CC_SIZE = 1
-    AVG_PATH_LENGTH = 2
     HARMONIC_AVG_PATH_LENGTH = 3
-    ROBUSTNESS = 4
 
     mapping = {
-        SUM_SQUARE_CC_SIZE: sum_square_connectivity_componenet_fitness_function,
-        AVG_PATH_LENGTH: avg_on_paths_length_fitness_function,
-        HARMONIC_AVG_PATH_LENGTH: harmonic_avg_on_paths_length_fitness_function,
-        ROBUSTNESS: robustness_fitness_function
+        SUM_SQUARE_CC_SIZE: (sum_square_connectivity_componenet_fitness_function, Optimum.MAX),
+        HARMONIC_AVG_PATH_LENGTH: (harmonic_avg_on_paths_length_fitness_function, Optimum.MIN),
     }
 
     @staticmethod
     def get_fitness_function(ff):
-        return FitnessFunctions.mapping.get(ff)
+        ff_info = FitnessFunctions.mapping.get(ff)
+        if ff_info:
+            return ff_info[0]
+        return None
 
-
-
+    @staticmethod
+    def get_fitness_function_optimum(ff):
+        ff_info = FitnessFunctions.mapping.get(ff)
+        if ff_info:
+            return ff_info[1]
+        return None